9df165882dd7ed96fd1ebdb01f7499dbe99765da
Benjamin Richter <aw17514@Admins-MacBook-Pro.local>
2018-12-13 13:56:39 +0100

    sending status

--- step.sh
+++ step.sh
@@ -2,20 +2,20 @@
 set -ex
 
 echo "Sending status ${bitbucket_build_status} to bitbucket."
-URL="${bitbucket_status_url}/${BITRISE_GIT_COMMIT}"
+URL="${BITBUCKET_STATUS_URL}/${BITRISE_GIT_COMMIT}"
 BITBUCKET_STATUS=$(cat <<EOF
 {
   "state": "$bitbucket_build_status",
 	"key": "BITRISE",
-	"url": "$bitbucket_status_url"
+	"url": "$BITRISE_BUILD_URL"
 }
 EOF
 )
 
 echo $URL
 echo $BITBUCKET_STATUS
-# curl -X POST \
-#   $URL \
-#   -u $BITBUCKET_AUTH \
-#   -H 'Content-Type: application/json' \
-#   -d "$BITBUCKET_STATUS" \
+curl -X POST \
+  $URL \
+  -u $BITBUCKET_AUTH \
+  -H 'Content-Type: application/json' \
+  -d "$BITBUCKET_STATUS" \
